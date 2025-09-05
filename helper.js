321411aa696b661ea510825f20912118f7bd8e69
iophantonie <timobenk96@gmail.com>
2025-09-05 10:03:02 +0200

    Create helper.js

---
+++ helper.js
@@ -0,0 +1,13 @@
+var _h = {
+
+    doAlert: function(s_text) {
+        alert(s_text);
+    },
+
+    j: function(i) {
+        return (i-100)*0.15;
+    },
+    k: function(i){
+        return (i-88)*0.2;
+    }
+};
